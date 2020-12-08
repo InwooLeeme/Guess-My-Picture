739f2d16f6252d71aa6d6c8a6e5980da06f98d8a
ItsmeLee <arsmagica90@gmail.com>
2020-12-08 12:30:55 +0900

    Install eslint
    Installed eslint and fix some errors.

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-import {join } from "path";
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
 
 const app = express();
 const PORT = 4000;
 
-const handleGet = (req, res) =>{
-    res.render("home");
-}
+const handleGet = (req, res) => {
+  res.render("home");
+};
 
-app.set('view engine',"pug");
-app.set('views',join(__dirname + '/views'));
-app.use(express.static(join(__dirname,"static")));
-app.get('/',handleGet);
+app.set("view engine", "pug");
+app.set("views", join(__dirname + "/views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", handleGet);
 
-app.listen(PORT,() => {
-    console.log(`Server is running`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
