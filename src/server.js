a3f56e38bc039bae1ea1dd905690a6fca72cef2b
ItsmeLee <arsmagica90@gmail.com>
2020-12-08 20:19:30 +0900

    #2.0 Hello World part One
    Set up the server connection.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const app = express();
 const PORT = 4000;
@@ -15,9 +16,16 @@ const handleListening = () => {
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname + "/views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", handleGet);
 
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
