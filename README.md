c2e9df3e8fc1815e2ce89a46959bf8b3550a6bc7
ItsmeLee <arsmagica90@gmail.com>
2020-12-08 12:05:12 +0900

    Server Set Up
    Installed socketIO and pug.
    Set up the server

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Guess-My-Picture
-Realtime Drawing game built with SocketIO, GULP and Node 
+
+Realtime Drawing game built with SocketIO, GULP and Node
+
+# To Do
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [x] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
+import {join } from "path";
 import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+const handleGet = (req, res) =>{
+    res.render("home");
+}
+
+app.set('view engine',"pug");
+app.set('views',join(__dirname + '/views'));
+app.use(express.static(join(__dirname,"static")));
+app.get('/',handleGet);
+
+app.listen(PORT,() => {
+    console.log(`Server is running`);
+})
