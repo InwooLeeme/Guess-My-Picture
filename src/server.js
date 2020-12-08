0a07424cbe97bb9a742b3bc906bd6fec610f411c
ItsmeLee <arsmagica90@gmail.com>
2020-12-08 14:16:28 +0900

    Setting Up SocketIO
    Setting up SocketIO in server.js file.

--- src/server.js
+++ src/server.js
@@ -9,11 +9,15 @@ const handleGet = (req, res) => {
   res.render("home");
 };
 
+const handleListening = () => {
+  console.log(`Server is running`);
+};
+
 app.set("view engine", "pug");
 app.set("views", join(__dirname + "/views"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", handleGet);
 
-app.listen(PORT, () => {
-  console.log(`Server is running`);
-});
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
