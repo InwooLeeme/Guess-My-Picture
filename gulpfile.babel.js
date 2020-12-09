d08f7e3f225892c0f483981a38d0a32804a1ec29
ItsmeLee <arsmagica90@gmail.com>
2020-12-09 15:11:10 +0900

    Minify css file using gulp
    Minify css file using gulp and translate all scss file to css file.
    And Apply css file in the pug template.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,35 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import csso from "gulp-csso";
+
+sass.compiler = require("node-sass");
 
 const path = {
   styles: {
     src: "assets/scss/style.scss",
     dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",     //assets 폴더 아래 scss폴더 안에 있는 모든 scss 파일을 지켜봄.
   },
 };
 
-export function styles() {
+function styles() {
   return gulp
     .src(path.styles.src)
     .pipe(sass())
-    .pipe(gulp.dest(path.styles.dest));
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(csso())                           // minify (압축)함.
+    .pipe(gulp.dest(path.styles.dest));     // path.style.dest => src폴더 안의 static안의 styles안에 저장함.
 }
+
+function watchFiles(){
+  gulp.watch(path.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
